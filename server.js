c8c2d2554f42a0c6c8a8ed05baffc380e1429b6d
Ahmed Elsalloul <salo0ol@ahmeds-mbp.home>
2020-05-10 08:43:27 +1000

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 3500;
+const server = app.listen(port, ()=>{console.log(`listening on port number ${port}`)});
+
+// GET Route
+app.get('/', function(req, res){
+    res.send(projectData)
+})
+
+// POST Route
+app.post('/', function(req, res){
+    let data = req.body;
+    projectData['temperature'] = data.temperature;
+    projectData['date'] = data.date;
+    projectData['user-response'] = data.user-response;
+    res.send('POST Request to the Server App')
+})
---
+++ website/app.js
@@ -0,0 +1,44 @@
+/* Global Variables */
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = ',US&appid=70ffa5e360831848a703aac925439b64';
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// http://api.openweathermap.org/data/2.5/weather?zip=94040,US&appid=70ffa5e360831848a703aac925439b64
+
+// select the generate item and add listener
+document.getElementById('generate').addEventListener('click', performAction);
+
+// the main action function
+function performAction(e){
+    // get the value of the zip item    
+    const zipValue = document.getElementById('zip').value;
+
+    // make call to the api
+    getTemp(baseURL, zipValue, apiKey);
+}
+
+// get temp from the api
+const getTemp = async (apiURL, zip, key)=> {
+    const fullURL = apiURL + zip + key;
+    const response = await fetch(fullURL, {
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'content-type': 'application/json',
+        }
+    });
+    try {
+        const newData = response.json();
+        console.log(newData);
+    } catch(error) {
+        console.log('error', error);
+    }
+} 
+
+// I've added few lines to compare
+
+
